8297730d85a224e92b4bcbb26175b9b4e2fe8850
Barroso Filho <barrosofilho.ti@gmail.com>
2023-03-30 02:05:11 -0300

    📃 docs: wait-for-it (credits)

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Use this script to test if a given TCP host/port are available
 
+# Source: https://github.com/codeedu/docker-wait-for-it
+
 WAITFORIT_cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
