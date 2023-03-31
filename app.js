1fd420653147d2d23ae7cb4ba5ec087259678043
Barroso Filho <barrosofilho.ti@gmail.com>
2023-03-31 19:55:50 -0300

    🦄 refactor: delete init.sql and init database by app.js

--- app.js
+++ app.js
@@ -1,61 +1,62 @@
-const express = require("express");
-const mysql = require("mysql2/promise");
-
+const express = require('express');
+const mysql = require('mysql2/promise');
 class Database {
   constructor(config) {
-    this.config = config;
+    this.pool = mysql.createPool({
+      host: config.host,
+      user: config.user,
+      password: config.password,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    });
+    this.database = config.database;
   }
 
-  async connect() {
-    return await mysql.createConnection(this.config);
-  }
+  async createDatabaseIfNotExists() {
+    const [rows] = await this.pool.query(`
+      SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = '${this.database}'
+    `);
+    if (rows[0]["COUNT(*)"] === 0) {
+      await this.pool.query(`CREATE DATABASE ${this.database}`);
+    }
+    await this.pool.query(`USE ${this.database}`);
+  }  
 
   async createTableIfNotExists() {
-    const connection = await this.connect();
-    const [rows] = await connection.execute(`
-      SELECT COUNT(*)
-      FROM information_schema.tables
-      WHERE table_schema = '${this.config.database}' AND table_name = 'people'
-    `);
+    const [rows] = await this.pool.execute(`
+  SELECT COUNT(*)
+  FROM information_schema.tables
+  WHERE table_schema = '${this.database}' AND table_name = 'people'
+`);
 
-    if (rows[0]["COUNT(*)"] === 0) {
-      await connection.execute(`
-        CREATE TABLE people (
-          id INT AUTO_INCREMENT PRIMARY KEY,
-          name VARCHAR(255) NOT NULL
-        )
-      `);
-    }
-
-    await connection.end();
+if (rows[0]["COUNT(*)"] === 0) {
+  await this.pool.execute(`
+    CREATE TABLE people (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
+    )
+  `);
+}
   }
 }
 
 class PeopleRepository {
   constructor(database) {
-    this.database = database;
+    this.pool = database.pool;
   }
 
-  async insertPeople(name) {
-    const connection = await this.database.connect();
-    await connection.execute("INSERT INTO people (name) VALUES (?)", [name]);
-    await connection.end();
+  async insertPerson(name) {
+    await this.pool.execute("INSERT INTO people (name) VALUES (?)", [name]);
   }
 
   async getPeople() {
-    const connection = await this.database.connect();
-    const [rows] = await connection.execute("SELECT name FROM people");
-    await connection.end();
+    const [rows] = await this.pool.execute('SELECT name FROM people');
     return rows;
   }
 
-  async checkIfpeopleExists(name) {
-    const connection = await this.database.connect();
-    const [rows] = await connection.execute(
-      "SELECT COUNT(*) FROM people WHERE name = ?",
-      [name]
-    );
-    await connection.end();
+  async checkIfPersonExists(name) {
+    const [rows] = await this.pool.execute('SELECT COUNT(*) FROM people WHERE name = ?', [name]);
     return rows[0]["COUNT(*)"] > 0;
   }
 }
@@ -69,20 +70,21 @@ class App {
 
   async handleRequest(req, res) {
     const name = req.query.name || "Barroso Filho";
-    const exists = await this.peopleRepository.checkIfpeopleExists(name);
+    const exists = await this.peopleRepository.checkIfPersonExists(name);
     if (!exists) {
-      await this.peopleRepository.insertPeople(name);
+      await this.peopleRepository.insertPerson(name);
     }
     const people = await this.peopleRepository.getPeople();
     let response = "<h1>Full Cycle Rocks!</h1>";
-    response += `<p>Você pode facilmente adicionar novos users via GET Params. Exemplo: <em><code>/?name=Wesley</code></em></p>`;
+    response += `<p>Você pode facilmente adicionar novos usuários via GET Params. Exemplo: <em><code>/?name=Wesley</code></em></p>`;
     response += "<ul>";
-    people.forEach((person) => {
+    for (const person of people) {
       response += `<li>${person.name}</li>`;
-    });
+    }
     response += "</ul>";
     res.send(response);
   }
+  
 
   start(port) {
     this.app.listen(port, () => {
@@ -98,16 +100,15 @@ const dbConfig = {
   database: "desafio_nginx_node",
 };
 
-const database = new Database(dbConfig);
-const peopleRepository = new PeopleRepository(database);
-const app = new App(peopleRepository);
-const PORT = 3000;
-
 (async () => {
   try {
+    const database = new Database(dbConfig);
+    await database.createDatabaseIfNotExists();
     await database.createTableIfNotExists();
-    app.start(PORT);
-  } catch (err) {
-    console.error("Error creating database or table:", err);
+    const peopleRepository = new PeopleRepository(database);
+    const app = new App(peopleRepository);
+    app.start(3000);
+  } catch (error) {
+    console.error(error);
   }
 })();
